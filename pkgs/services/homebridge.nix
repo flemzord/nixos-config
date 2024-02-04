8205b614c82dd8dcd339664b2ee6f25a0a1c32ec
Maxence Maireaux <maxence@maireaux.fr>
2024-02-04 19:45:20 +0100

    feat: Add homebridge

--- pkgs/services/homebridge.nix
+++ pkgs/services/homebridge.nix
@@ -2,7 +2,7 @@
   virtualisation.oci-containers.containers = {
     homebridge = {
       image = "homebridge/homebridge:latest";
-      networkMode = "host";
+      extraOptions = [ "--network=host" ];
       volumes = [ "homebridge:/homebridge" ];
       autoStart = true;
     };
