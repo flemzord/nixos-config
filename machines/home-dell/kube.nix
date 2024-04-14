947190fbbf89c71468af7b8de1578a9d74a0ae8c
Maxence Maireaux <maxence@maireaux.fr>
2024-04-14 15:17:44 +0700

    feat: Remove few service in k3s cluster

--- machines/home-dell/kube.nix
+++ machines/home-dell/kube.nix
@@ -4,5 +4,6 @@
     role = "server";
     token = "mysupertokenwhichshouldnotbepublicbutwillbeanyway";
     clusterInit = true;
+    extraFlags = "--disable=servicelb --disable-helm-controller --disable=traefik"
   };
 }
--- machines/home-hp/kube.nix
+++ machines/home-hp/kube.nix
@@ -4,5 +4,6 @@
     role = "server";
     token = "mysupertokenwhichshouldnotbepublicbutwillbeanyway";
     serverAddr = "https://192.168.1.119:6443";
+    extraFlags = "--disable=servicelb --disable-helm-controller --disable=traefik"
   };
 }
