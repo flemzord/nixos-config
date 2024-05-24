e412b2979a299fa951c16cff937cfd55fedc73bc
Maxence Maireaux <maxence@maireaux.fr>
2024-05-24 09:51:47 +0200

    chore: Add config

--- pkgs/services/cloudflared.nix
+++ pkgs/services/cloudflared.nix
@@ -4,6 +4,17 @@
   services = {
     cloudflared = {
       enable = true;
+      tunnels = {
+        "01eea35c-34c5-42c0-811c-ad23cf8bc655" = {
+          credentialsFile = "/etc/nixos/secrets/01eea35c-34c5-42c0-811c-ad23cf8bc655.json";
+          default = "http_status:404";
+          ingress = {
+            "n8n.flemzord.ovh" = {
+              service = "http://localhost:5678"
+            }
+          }
+        }
+      }
     };
   };
 }
