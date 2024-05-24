770b4338f7098ac12f7fab2c4e604d36b19ed8ed
Maxence Maireaux <maxence@maireaux.fr>
2024-05-24 09:19:11 +0200

    chore: Upgrade n8n config

--- pkgs/services/n8n.nix
+++ pkgs/services/n8n.nix
@@ -1,13 +1,18 @@
 {
   virtualisation.oci-containers.containers = {
     n8n = {
-      image = "docker.n8n.io/n8nio/n8n:1.40.0";
+      image = "docker.n8n.io/n8nio/n8n:1.42.1";
       ports = [ "5678:5678" ];
       volumes = [ "n8n_data:/home/node/.n8n" ];
       environment = {
         N8N_SECURE_COOKIE = "false";
         GENERIC_TIMEZONE = "Europe/Paris";
         TZ = "Europe/Paris";
+        N8N_HOST = "n8n.flemzord.ovh";
+        N8N_PORT = "5678";
+        N8N_PROTOCOL = "https";
+        WEBHOOK_URL = "https://n8n.flemzord.ovh/"
+
       };
       autoStart = true;
     };
