06f8078e7ad63eef66e8673711ad16747e8be79c
Maxence Maireaux <maxence@maireaux.fr>
2024-10-18 10:28:19 +0200

    chore: Update config for home-hp

--- machines/home-hp/default.nix
+++ machines/home-hp/default.nix
@@ -42,14 +42,20 @@
     };
   };
 
-    services.xserver.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
-    environment.gnome.excludePackages = with pkgs; [
-      gnome-tour
-      gnome-connections
-      epiphany # web browser
-      gnome.geary # email reader. Up to 24.05. Starting from 24.11 the package name is just geary.
-      evince # document viewer
-    ];
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-connections
+    epiphany # web browser
+    geary # email reader. Up to 24.05. Starting from 24.11 the package name is just geary.
+    evince # document viewer
+  ];
+  i18n.supportedLocales = ["all"];
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "flemzord";
+security.chromiumSuidSandbox.enable = true;
+ # systemd.services."getty@tty1".enable = false;
+#  systemd.services."autovt@tty1".enable = false;
 }
