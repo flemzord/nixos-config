d0200244285fdefc8c6a5cf0ce7d38464f966b8e
Maxence Maireaux <maxence@maireaux.fr>
2024-12-30 15:09:17 +0100

    feat: Add Qdrant in Dell serveur

--- pkgs/services/qdrant.nix
+++ pkgs/services/qdrant.nix
@@ -1,18 +1,20 @@
 {
   services.qdrant = {
     enable = true;
-    hsnw_index = {
-      on_disk = true;
+    settings = {
+      hsnw_index = {
+        on_disk = true;
+      };
+      service = {
+        host = "0.0.0.0";
+        http_port = 6333;
+        grpc_port = 6334;
+      };
+      storage = {
+        snapshots_path = "/var/lib/qdrant/snapshots";
+        storage_path = "/var/lib/qdrant/storage";
+      };
+      telemetry_disabled = true;
     };
-    service = {
-      host = "0.0.0.0";
-      http_port = 6333;
-      grpc_port = 6334;
-    };
-    storage = {
-      snapshots_path = "/var/lib/qdrant/snapshots";
-      storage_path = "/var/lib/qdrant/storage";
-    };
-    telemetry_disabled = true;
   };
 }
