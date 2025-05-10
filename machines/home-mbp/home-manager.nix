ba703fe5f432039e1ccb2d7e30b1f96ff51f7de8
Maxence Maireaux <maxence@maireaux.fr>
2025-05-10 22:45:22 +0200

    Refactor home-manager configuration to rename 'initExtraFirst' to 'initContent' for clarity and consistency across configurations.

--- machines/home-mbp/home-manager.nix
+++ machines/home-mbp/home-manager.nix
@@ -72,7 +72,7 @@ in
           plugins = [
 
           ];
-          initExtraFirst = ''
+          initContent = lib.mkBefore ''
             if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
               . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
               . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
