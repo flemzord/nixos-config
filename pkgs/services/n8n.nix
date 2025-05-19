17e3192aeb4dcd4cbc698f822ef0d72f034f40a0
Maxence Maireaux <maxence@maireaux.fr>
2025-05-19 12:36:26 +0200

    Update n8n Docker image version from 1.88.0 to 1.92.2

--- pkgs/services/n8n.nix
+++ pkgs/services/n8n.nix
@@ -1,7 +1,7 @@
 {
   virtualisation.oci-containers.containers = {
     n8n = {
-      image = "docker.n8n.io/n8nio/n8n:1.88.0";
+      image = "docker.n8n.io/n8nio/n8n:1.92.2";
       ports = [ "5678:5678" ];
       volumes = [ "n8n_data:/home/node/.n8n" ];
       environment = {
