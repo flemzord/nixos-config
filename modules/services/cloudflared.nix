54151a827cf5f3416146cc30ada97c024ab3fc6f
Maxence Maireaux <maxence@maireaux.fr>
2025-10-27 11:30:40 +0100

    fix: Update ingress configuration for cloudflared service

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -9,8 +9,8 @@ _:
           credentialsFile = "/etc/nixos/secrets/01eea35c-34c5-42c0-811c-ad23cf8bc655.json";
           default = "http_status:404";
           ingress = {
-            "n8n.flemzord.ovh" = {
-              service = "http://localhost:5678";
+            "ha.flemzord.ovh" = {
+              service = "http://localhost:8123";
             };
           };
         };
