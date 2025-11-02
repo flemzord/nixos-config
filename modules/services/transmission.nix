68d4a29b0d72b143dc069b9ae896bb3ed8692b16
Maxence Maireaux <maxence@maireaux.fr>
2025-11-02 23:46:32 +0100

    fix: Upgrade Transmission

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -1,4 +1,4 @@
-_:
+{ pkgs, ... }:
 
 let
   downloadDir = "/srv/media";
@@ -14,6 +14,7 @@ in
 
   services.transmission = {
     enable = true;
+    package = pkgs.transmission_4;
     user = runUser;
     group = runGroup;
     openFirewall = true;
