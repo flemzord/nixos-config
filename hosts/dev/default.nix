924b877b517e2d45705e6c9fec86e86ae6b385a2
Maxence Maireaux <maxence@maireaux.fr>
2026-02-13 10:05:34 +0100

    fix: use home.file for SSH authorized_keys with RSA key

--- hosts/dev/default.nix
+++ hosts/dev/default.nix
@@ -28,12 +28,8 @@ in
 
   };
 
-  home.activation.authorizedKeys = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "$HOME/.ssh" && chmod 700 "$HOME/.ssh"
-    cat > "$HOME/.ssh/authorized_keys" << 'SSH_KEYS'
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHvSrBVrcw0wYewYB5RKkr2RTQ2aUyP74jFNZgR1YKb maxence@maireaux.fr
-SSH_KEYS
-    chmod 600 "$HOME/.ssh/authorized_keys"
+  home.file.".ssh/authorized_keys".text = ''
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC33/UmOxIFBgPxxmr2qVqhN7wgdTLriKg4Em7MLi5KeIfWHs+Jqp7Fh6QDWwyOtRz8ARqtVlfZrO00xRAHx5UQkXmbd1iXeQgg7FPV+KuyAvAyfqciq0MJXFo5lIA9eO9TyFUKzC4dI/ayOubQDB8v5tCd+gYsW35eDrO5ueLi7ld2Q04lBO2mTNKoX0JUAd4+FYe9zkBXClh9ik0+F2IRBgG9HTVNqObUfXtpHp4iW0avXn7Syc4079rIkrwup7Swkxy1uo5nYeJSPHgnhDzjeCxzIal0UIDmPBHLAiuf8r2yWFb689jrmyfLYqN+o8QR2A5n+xQ5yxGmBDFKgkGN
   '';
 
   # Marked broken Oct 20, 2022 check later to remove this
