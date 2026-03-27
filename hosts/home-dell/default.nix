25e2d6a3ab50f5e6460ee86672af76942c43d9c6
Maxence Maireaux <maxence@maireaux.fr>
2026-03-27 21:35:10 +0100

    fix(home-dell): remove unused postgresql service
    postgresql-setup.service was stuck in an infinite auth loop because
    pg_hba.conf required md5 but the postgres user had no password set.
    The database is no longer needed on this server.

--- hosts/home-dell/default.nix
+++ hosts/home-dell/default.nix
@@ -23,30 +23,6 @@
 
   environment.systemPackages = pkgs.callPackage ./packages.nix { };
 
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_17;
-    ensureDatabases = [ "postgres" "kipli" ];
-    ensureUsers = [
-      {
-        name = "postgres";
-        ensureDBOwnership = true;
-      }
-    ];
-    authentication = pkgs.lib.mkOverride 10 ''
-      local all all md5
-      host all all 127.0.0.1/32 md5
-      host all all ::1/128 md5
-    '';
-    settings = {
-      listen_addresses = "localhost";
-      port = 5432;
-    };
-    initialScript = pkgs.writeText "backend-initScript" ''
-      ALTER USER postgres PASSWORD 'postgres';
-    '';
-  };
-
   services.nixos-auto-update = {
     enable = true;
     hostname = "home-dell";
