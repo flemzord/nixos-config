b5be205ad3d60729528168368bc2506ec0da22c5
Maxence Maireaux <maxence@maireaux.fr>
2026-04-03 21:22:07 +0200

    feat: Add home-dell host key and rekey all secrets

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,7 @@
 let
   # User SSH public keys (for encrypting secrets)
   flemzord = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC33/UmOxIFBgPxxmr2qVqhN7wgdTLriKg4Em7MLi5KeIfWHs+Jqp7Fh6QDWwyOtRz8ARqtVlfZrO00xRAHx5UQkXmbd1iXeQgg7FPV+KuyAvAyfqciq0MJXFo5lIA9eO9TyFUKzC4dI/ayOubQDB8v5tCd+gYsW35eDrO5ueLi7ld2Q04lBO2mTNKoX0JUAd4+FYe9zkBXClh9ik0+F2IRBgG9HTVNqObUfXtpHp4iW0avXn7Syc4079rIkrwup7Swkxy1uo5nYeJSPHgnhDzjeCxzIal0UIDmPBHLAiuf8r2yWFb689jrmyfLYqN+o8QR2A5n+xQ5yxGmBDFKgkGN";
+  home-dell = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqvBkcAzQV4A5Cx3+6+hoHHnUnlshJx82EIC/fOwBGK root@home-dell";
 
   # Host SSH public keys (ed25519 host keys from /etc/ssh/ssh_host_ed25519_key.pub)
   # Run on each host: cat /etc/ssh/ssh_host_ed25519_key.pub
@@ -15,7 +16,7 @@ let
   # systems = [ home-hp home-dell dev-server ];
 
   # All keys (users + systems)
-  allKeys = users; # ++ systems;
+  allKeys = users ++ [ home-dell ];
 in
 {
   # Cloudflare secrets
