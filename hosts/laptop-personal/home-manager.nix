91899f671c8d075f71b4cf38015074475071998f
Maxence Maireaux <maxence@maireaux.fr>
2026-05-01 11:44:53 +0200

    Revert "feat(hermes): manage SOUL.md with home-manager"
    This reverts commit e77e9ef76e7c3f86c1da479c444cbc1bd6c55ec7.

--- hosts/laptop-personal/home-manager.nix
+++ hosts/laptop-personal/home-manager.nix
@@ -24,7 +24,6 @@ in
           ./../../modules/home-manager/programs/vim.nix
           ./../../modules/home-manager/programs/claude-code.nix
           ./../../modules/home-manager/programs/codex.nix
-          ./../../modules/home-manager/programs/hermes.nix
           ./../../modules/home-manager/programs/aerospace.nix
           ./../../modules/home-manager/programs/glow.nix
           ./../../modules/home-manager/programs/direnv.nix
