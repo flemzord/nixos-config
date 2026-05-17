a32d50e61601c70cd81b902c45e218a7e76e2bea
Maxence Maireaux <maxence@maireaux.fr>
2026-05-17 20:36:27 +0200

    fix(fail2ban): harden ssh jail on server dev

--- modules/services/fail2ban.nix
+++ modules/services/fail2ban.nix
@@ -3,23 +3,25 @@ _:
 {
   services.fail2ban = {
     enable = true;
-    maxretry = 4;
-    bantime = "1h";
+    maxretry = 3;
+    bantime = "2h";
     ignoreIP = [
       "127.0.0.1/8"
       "::1"
     ];
     bantime-increment = {
       enable = true;
-      maxtime = "24h";
+      maxtime = "7d";
       overalljails = true;
     };
     jails.sshd.settings = {
+      backend = "systemd";
       enabled = true;
+      filter = "sshd";
+      findtime = "15m";
+      maxretry = 3;
       mode = "aggressive";
-      maxretry = 4;
-      findtime = "10m";
-      bantime = "1h";
+      bantime = "2h";
     };
   };
 }
